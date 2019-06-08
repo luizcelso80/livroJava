ead20a09e6930a7d45001ddb6645f00170e37548
Luiz <dev@limabauru.com.br>
2019-06-08 15:52:11 -0300

    Controller wrong

--- src/main/java/br/biblioteca/livros/controllers/LivroController.java
+++ src/main/java/br/biblioteca/livros/controllers/LivroController.java
@@ -1,29 +1,5 @@
 package br.biblioteca.livros.controllers;
 
 public class LivroController {
-	
-	private Long id;
-	private String nome;
-	private Integer quantidadePaginas;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public Integer getQuantidadePaginas() {
-		return quantidadePaginas;
-	}
-	public void setQuantidadePaginas(Integer quantidadePaginas) {
-		this.quantidadePaginas = quantidadePaginas;
-	}
-	
-	
 
 }
