77135b9c50a80a8c52d6562c96c7249c35e8524d
Luiz <dev@limabauru.com.br>
2019-07-20 10:52:17 -0300

    config postgre

--- src/main/java/br/biblioteca/livros/controllers/AutorController.java
+++ src/main/java/br/biblioteca/livros/controllers/AutorController.java
@@ -3,12 +3,14 @@ package br.biblioteca.livros.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.biblioteca.livros.entidades.Autor;
+import br.biblioteca.livros.entidades.Livro;
 import br.biblioteca.livros.services.AutorService;
 
 @Controller
@@ -25,7 +27,7 @@ public class AutorController {
 	}
 	
 	@GetMapping("novo")
-	public ModelAndView create() {
+	public ModelAndView create(@ModelAttribute Autor autor) {
 		System.out.println("criei");
 		return new ModelAndView("/autores/create");
 	}
